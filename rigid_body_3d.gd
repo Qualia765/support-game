3eaee80267fe3809be9a31aaf7e0d08cbd41909a
Qualia <qualia@team-farrell.com>
2026-05-23 08:22:24 -0700

    player controller

--- rigid_body_3d.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody3D
-
-@export var speed: float
-
-@onready var camera_3d: Camera3D = $Camera3D
-
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	linear_velocity += basis * Vector3(Input.get_axis("left", "right"), 0, Input.get_axis("foward", "back")) * speed
